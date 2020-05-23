b29a6f48130b5e0879608d8e92d1c2f54755cff8
Felix Bauckholt <felixbauckholt@gmail.com>
2020-05-23 10:55:26 -0500

    Apply rustfmt to get rid of all warnings

--- src/strategies/examples.rs
+++ src/strategies/examples.rs
@@ -1,5 +1,5 @@
-use strategy::*;
-use game::*;
+use crate::game::*;
+use crate::strategy::*;
 use rand::{self, Rng};
 
 // dummy, terrible strategy, as an example
@@ -10,7 +10,7 @@ pub struct RandomStrategyConfig {
 }
 
 impl GameStrategyConfig for RandomStrategyConfig {
-    fn initialize(&self, _: &GameOptions) -> Box<GameStrategy> {
+    fn initialize(&self, _: &GameOptions) -> Box<dyn GameStrategy> {
         Box::new(RandomStrategy {
             hint_probability: self.hint_probability,
             play_probability: self.play_probability,
@@ -23,7 +23,7 @@ pub struct RandomStrategy {
     play_probability: f64,
 }
 impl GameStrategy for RandomStrategy {
-    fn initialize(&self, player: Player, _: &BorrowedGameView) -> Box<PlayerStrategy> {
+    fn initialize(&self, player: Player, _: &BorrowedGameView) -> Box<dyn PlayerStrategy> {
         Box::new(RandomStrategyPlayer {
             hint_probability: self.hint_probability,
             play_probability: self.play_probability,
@@ -44,7 +44,9 @@ impl PlayerStrategy for RandomStrategyPlayer {
         if p < self.hint_probability {
             if view.board.hints_remaining > 0 {
                 let hint_player = view.board.player_to_left(&self.me);
-                let hint_card = rand::thread_rng().choose(&view.get_hand(&hint_player)).unwrap();
+                let hint_card = rand::thread_rng()
+                    .choose(&view.get_hand(&hint_player))
+                    .unwrap();
                 let hinted = {
                     if rand::random() {
                         // hint a color
@@ -66,6 +68,5 @@ impl PlayerStrategy for RandomStrategyPlayer {
             TurnChoice::Discard(0)
         }
     }
-    fn update(&mut self, _: &TurnRecord, _: &BorrowedGameView) {
-    }
+    fn update(&mut self, _: &TurnRecord, _: &BorrowedGameView) {}
 }
